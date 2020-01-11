04ef332f2538a5c9d45b4d264a8f28b7521c8897
Ariful <arifsofg@gmail.com>
2020-01-12 03:28:52 +0600

    changes

--- application/views/web/lib/header.php
+++ application/views/web/lib/header.php
@@ -5,17 +5,20 @@
     <meta charset="utf-8">
     <meta name="siteurl" content="https://learn24bd.com" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="<?php echo base_url();?>assets/front/img/logo.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/front/img/logo.png" type="image/x-icon" />
     <title> Diu - Project Archive, A Collaborive Platform </title>
-    <meta name="description" content="learn24bd offer you Bangla tutorial on web development, wordpress, laravel framework, flutter, android, how to questions,tips and tricks, resources and more.">
-    <meta name="keywords" content="bangla tutorial, web development, laravel bangla tutorial, flutter bangla tutorial, tips and tricks, wordpress snippet, javascript bangla tutorial">
-    <link rel="canonical" href="https://learn24bd.com" />
+    <meta name="description"
+        content="learn24bd offer you Bangla tutorial on web development, wordpress, laravel framework, flutter, android, how to questions,tips and tricks, resources and more.">
+    <meta name="keywords"
+        content="bangla tutorial, web development, laravel bangla tutorial, flutter bangla tutorial, tips and tricks, wordpress snippet, javascript bangla tutorial">
+    <link rel="canonical" href="<?php echo base_url(); ?>" />
     <link href="<?php echo base_url(); ?>assets/front/file/css/bundle.min.css" rel="stylesheet" />
-    <link href="<?php echo base_url(); ?>assets/front/fonts/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="<?php echo base_url(); ?>assets/front/fonts/font-awesome-4.7.0/css/font-awesome.min.css"
+        rel="stylesheet" />
     <link href="<?php echo base_url(); ?>assets/front/file/css/style.min.css" rel="stylesheet" />
     <!-- end bunlde stylesheet -->
 
-    
+
 </head>
 
 <body>
@@ -28,61 +31,83 @@
                         <div class="navbar-header">
                             <!-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button> -->
                             <div id="logo">
-                                <a href="<?php echo base_url(); ?>"> <img src="<?php echo base_url(); ?>assets/front/img/logo.png" alt="logo of learn24bd" title="diu logo" style="height: 80px; width: 150px;"></a>
+                                <a href="<?php echo base_url(); ?>"> <img
+                                        src="<?php echo base_url(); ?>assets/front/img/logo.png" alt="logo of learn24bd"
+                                        title="diu logo" style="height: 80px; width: 150px;"></a>
                             </div>
                         </div>
                         <div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">
                             <ul id="main-menu" class="nav navbar-nav">
-                                <li class="hvr-underline-from-left"> <a title="Home" href="<?php echo base_url(); ?>"><i class="fa fa-home"></i>&nbsp;Home</a> </li>
+                                <li class="hvr-underline-from-left"> <a title="Home" href="<?php echo base_url(); ?>"><i
+                                            class="fa fa-home"></i>&nbsp;Home</a> </li>
 
-                                <li class="hvr-underline-from-left"> <a title="Project" href="<?php echo base_url(); ?>project">Project</a> </li>
-                                
-                                <?php if($this->session->userdata('student') ): ?>
-                                    <li class="hvr-underline-from-left"> <a title="Profile" href="<?php echo base_url(); ?>student/profile/<?php echo $this->session->student_username; ?>">Profile</a> </li>
+                                <li class="hvr-underline-from-left"> <a title="Project"
+                                        href="<?php echo base_url(); ?>project">Project</a> </li>
+
+                                <?php if ($this->session->userdata('student')) : ?>
+                                <li class="hvr-underline-from-left"> <a title="Profile"
+                                        href="<?php echo base_url(); ?>student/profile/<?php echo $this->session->student_username; ?>">Profile</a>
+                                </li>
                                 <?php endif; ?>
 
-                                <li class="hvr-underline-from-left"> <a title="Blog" href="<?php echo base_url(); ?>upload"><i class="fa fa-upload"></i>&nbsp;Upload</a> </li>
+                                <li class="hvr-underline-from-left"> <a title="Blog"
+                                        href="<?php echo base_url(); ?>upload"><i
+                                            class="fa fa-upload"></i>&nbsp;Upload</a> </li>
 
-                                <li class="hvr-underline-from-left"> <a title="Blog" href="<?php echo base_url(); ?>department">Department</a> </li>
-                                <?php if($this->session->userdata('student') ): ?>
-                                    <li class="hvr-underline-from-left"> <a title="Click to logout" href="<?php echo base_url(); ?>student/logout"><i class="fa fa-sign-out"></i>&nbsp;</a> </li>
-
-
-
-                                    <?php else: ?> 
-
-
-                                        <li class="hvr-underline-from-left"> <a title="Blog" href="<?php echo base_url(); ?>blog/1"><i class="fa fa-newspaper"></i>&nbsp;Blog</a> </li>
-
-                                  <?php endif; ?>
-                              </ul>
+                                <li class="hvr-underline-from-left"> <a title="Blog"
+                                        href="<?php echo base_url(); ?>department">Department</a> </li>
+                                <?php if ($this->session->userdata('student')) : ?>
+                                <li class="hvr-underline-from-left"> <a title="Click to logout"
+                                        href="<?php echo base_url(); ?>student/logout"><i
+                                            class="fa fa-sign-out"></i>&nbsp;</a> </li>
 
 
-                          </div>
+
+                                <?php else : ?>
 
 
-                      </nav>
-                  </div>
-                  <!-- header menu end -->
-                  <div class="col-md-4 col-xs-12">
+                                <li class="hvr-underline-from-left"> <a title="Blog"
+                                        href="<?php echo base_url(); ?>blog/1"><i
+                                            class="fa fa-newspaper"></i>&nbsp;Blog</a> </li>
+
+                                <?php endif; ?>
+                            </ul>
+
+
+                        </div>
+
+
+                    </nav>
+                </div>
+                <!-- header menu end -->
+                <div class="col-md-4 col-xs-12">
                     <form class="search" action="#" method="GET">
                         <div class="input-group">
-                            <input autocomplete="off" name="query" required="required" type="text" class="form-control" placeholder="Search Here">
+                            <input autocomplete="off" name="query" required="required" type="text" class="form-control"
+                                placeholder="Search Here">
                             <div class="input-group-btn">
-                                <button class="btn btn-default" type="submit" style="margin-bottom: 10px;"> <i class="fa fa-search"></i> </button>
-                                <?php if($this->session->student): ?>
-                                    &nbsp; &nbsp;  &nbsp; &nbsp;  &nbsp; &nbsp;  
-                                    <img src="<?php echo base_url(); ?>uploads/student/<?php echo $this->session->student_image; ?>" style="width: 40px; height: 40px; border-radius: 100%; margin-left: 10px;" alt="">sdfsdfsdafsafsdafsda
+                                <button class="btn btn-default" type="submit" style="margin-bottom: 10px;"> <i
+                                        class="fa fa-search"></i> </button>
+                                <?php if ($this->session->student) : ?>
+                                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                                <img src="<?php echo base_url(); ?>uploads/student/<?php echo $this->session->student_image; ?>"
+                                    style="width: 40px; height: 40px; border-radius: 100%; margin-left: 10px;"
+                                    alt="">sdfsdfsdafsafsdafsda
 
-                                    <?php else: ?>
+                                <?php else : ?>
 
-                                        &nbsp; &nbsp; <span style="margin-left: 10px;"><a href="<?php echo base_url();  ?>student" class="btn btn-sm"><strong>Login</strong></a><span style="color:red !important;">/</span><a href="<?php echo base_url();  ?>student/register" class="btn btn-sm"><strong>Register</strong></a></span>
+                                &nbsp; &nbsp; <span style="margin-left: 10px;"><a
+                                        href="<?php echo base_url();  ?>student"
+                                        class="btn btn-sm"><strong>Login</strong></a><span
+                                        style="color:red !important;">/</span><a
+                                        href="<?php echo base_url();  ?>student/register"
+                                        class="btn btn-sm"><strong>Register</strong></a></span>
 
-                                    <?php endif; ?>
-                                </div>
+                                <?php endif; ?>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
-        </header>
+        </div>
+    </header>
--- application/views/web/lib/hero.php
+++ application/views/web/lib/hero.php
@@ -7,17 +7,17 @@
 <div class="owl-carousel owl-theme">
 
     <div class="item">
-        <img src="https://daffodilvarsity.edu.bd/photos/slider/sqccs.png" style="height: 500px;" alt="">
+        <img src="<?php echo base_url(); ?>assets/front/img/slider/slider1.png" style="height: 500px;" alt="">
     </div>
 
-    
+
 
     <div class="item">
-        <img src="https://daffodilvarsity.edu.bd/photos/slider/transport.png" style="height: 500px;" alt="">
+        <img src="<?php echo base_url(); ?>assets/front/img/slider/slider2.png" style="height: 500px;" alt="">
     </div>
 
     <div class="item">
-        <img src="<?php echo base_url(); ?>assets/front/img/bg-img/diu.jpg" style="height: 500px;" alt="">
+        <img src="<?php echo base_url(); ?>assets/front/img/slider/slider3.png" style="height: 500px;" alt="">
     </div>
 
 
@@ -26,23 +26,23 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/front/css/owl/owl.carousel.min.js"></script>
 <script>
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin:10,
-        autoplaySpeed:true,
-        autoplay:true,
-        animateOut: 'slideOutDown',
-        animateIn: 'flipInX',
-        responsive:{
-            0:{
-                items:1
-            },
-            1000:{
-                items:3
-            },
-            1000:{
-                items:1
-            }
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplaySpeed: true,
+    autoplay: true,
+    animateOut: 'slideOutDown',
+    animateIn: 'flipInX',
+    responsive: {
+        0: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        },
+        1000: {
+            items: 1
         }
-    })
+    }
+})
 </script>
